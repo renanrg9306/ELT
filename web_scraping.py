94d6a21e03bf99f5912a66983eba7b8599bdf7dd
alfredorg9306@gmail.com <renanrg9306@gmail.com>
2025-09-14 14:59:35 -0600

    This code works perfectly fine. It includes extracting data from a table using Web Scraping.

---
+++ web_scraping.py
@@ -0,0 +1,36 @@
+import requests 
+import pandas as pd
+import re 
+from bs4 import BeautifulSoup
+
+# Step 1: Website you are getting the data from.
+url = "https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
+# Step 2: Make the get request to the website, use the .text to extract the content page.
+page_content = requests.get(url).text
+# Step 3 Create a soup Element using the BeautifulSoup Class
+soup = BeautifulSoup(page_content,'html.parser')
+# Step 4 Find the target table using soup object and pass the value as string: 'table' and class_ 'table class if case it has one'
+target_table = soup.find('table', class_='wikitable')
+
+
+# Step 4 If you table has any colspan 2, this will cause data inconsistency, so delete the class from the entire table.
+for td_tag in target_table.find_all('td'): # Combining two columns happens in the table data(td), Create a for loop that finds all the td tags.
+    for child_tag in td_tag.find_all('sup'): # Going through every td_tag en 
+        child_tag.decompose() # Remove from the tree the descendent childre of the tag.
+target_table.prettify() # We use prettify 
+
+target_table_tbody = target_table.find('tbody')  # Working only with the table body
+
+
+data = [] # variable when the row information will be saved.
+for row in target_table_tbody.find_all('tr'): # this loop is to go over every row.
+    row_data = [] # Temporary variable that will save the current value.
+    for cell in row.find_all(['td']): # Going over each cell in each row
+        colspan = int(cell.get('colspan',1)) # get colspan value, if not specified 
+        row_data.append(cell.get_text(strip=True))
+        for _ in range(colspan - 1):
+           row_data.append('')
+    data.append(row_data)
+
+df = pd.DataFrame(data=data,columns=['Country/Terretory','UN Region','Estimate1','Year1','Estimate2','Year2','Estimate3','Year3'])
+df.to_csv('./WebScraping.csv')
